d8d522963085a279c5327cd4bfecd10488b2441f
Yubin <yubin045@naver.com>
2026-04-23 22:26:34 +0900

    Fix: 검색 페이지 CSR 수정

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,26 +1,40 @@
 import SearchableLayout from '@/components/searchable-layout';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 // import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
+import { BookData } from '@/types';
+// import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext,
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: {
-      books,
-    },
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
+
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, []);
+
   return (
     <div>
       {books.map((book) => (
