36c3856b20cd2107f0b929521fbc179c49b4b31c
Yubin <yubin045@naver.com>
2026-04-23 22:26:05 +0900

    Feat: 메인 페이지 SSG 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import style from './index.module.css';
 import { ReactNode } from 'react';
 // import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomdBooks from '@/lib/fetch-random-books';
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+
+  console.log('인덱스 페이지');
 
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -27,7 +29,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
